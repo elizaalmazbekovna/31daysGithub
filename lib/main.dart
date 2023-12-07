ba7c02903a19d07dfa6780b4a0b2dcd2f0598db6
elizaalmazbekovna <eliza.almazbekovna01@gmail.com>
2023-12-07 18:48:49 +0600

    mini  Weather mobile app

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,168 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
+void main() => runApp(MyApp());
 
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
             title: Text(
-              'Example title',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .titleLarge,
+              "Weather",
+              style: TextStyle(color: Colors.black87),
             ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
             ),
-          ),
-        ],
+            iconTheme: IconThemeData(color: Colors.black54),
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+            ]),
+        body: _builtBody(),
       ),
     );
   }
 }
 
-void main() {
-  runApp(
-    const MaterialApp(
-      title: 'My app', // used by the OS task switcher
-      home: SafeArea(
-        child: MyScaffold(),
+Widget _builtBody() {
+  return SingleChildScrollView(
+      child: Column(
+    children: <Widget>[
+      _headerImage(),
+      SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _weatherDescription(),
+              Divider(),
+              _temparature(),
+              Divider(),
+              _temperatureForecast(),
+              Divider(),
+              _footerRatings(),
+            ],
+          ),
+        ),
       ),
-    ),
+    ],
+  ));
+}
+
+Image _headerImage() {
+  return Image(
+    image: NetworkImage(
+        'https://www.sknis.gov.kn/wp-content/uploads/2017/07/Weather-Blog-Image-700x375.jpg'),
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        "Wednesday - December 07",
+        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+      ),
+      Divider(),
+      Text(
+        "There are five climatic zones on this planet: the equatorial climate zone, the tropical climate zone, the temperate climatic zone, Arctic and Antarctic climate belts. Our country is in the temperate climatic zone.  ",
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    ],
+  );
+}
+
+Row _temparature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      SizedBox(width: 16.0),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                " 15° Clear",
+                style: TextStyle(color: Colors.deepPurple),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                " Чуйская область, Чуй",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10.0,
+    children: List.generate(8, (int index) {
+      return Chip(
+        label: Text(
+          '${index + 20}°C',
+          style: TextStyle(fontSize: 15.0),
+        ),
+        avatar: Icon(
+          Icons.wb_cloudy,
+          color: Colors.blue.shade300,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      Text(
+        'info with openweathermap.org',
+        style: TextStyle(fontSize: 15.0),
+      ),
+      stars,
+    ],
   );
 }
